9789f0c09bcfb009d215e90ea7061fc073f8d757
Risav03 <needleweed03@gmail.com>
2023-12-08 16:45:41 +0530

    font imported

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import candybeans from "next/font/local"
 
 const inter = Inter({ subsets: ['latin'] })
+const font = candybeans({src:"../assets/fonts/candy-beans.otf"})
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Navbar from '@/components/navbar'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <h1>TACO TRIVE DAPP</h1>
     </main>
   )
 }
