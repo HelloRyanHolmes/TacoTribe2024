d09b0d62d0473e66946f9269055862ab164c12eb
im-still-thinking <royaritra1990rocks@gmail.com>
2023-12-10 04:35:19 +0530

    Twitter

--- app/layout.js
+++ app/layout.js
@@ -40,6 +40,27 @@ export const metadata = {
     locale: 'en_US',
     type: 'website',
   },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Taco Tribe Dapp',
+    description: 'aco taco moreee tacooo',
+    siteId: '1467726470533754880',
+    creator: '@3xbuilds',
+    creatorId: '1467726470533754880',
+    images: ['https://d19rxn9gjbwl25.cloudfront.net/projectImages/home/background-image.png'],
+  },
+  app: {
+    name: 'twitter_app',
+    id: {
+      iphone: 'twitter_app://iphone',
+      ipad: 'twitter_app://ipad',
+      googleplay: 'twitter_app://googleplay',
+    },
+    url: {
+      iphone: 'https://iphone_url',
+      ipad: 'https://ipad_url',
+    },
+  },
 }
 
 export default function RootLayout({ children }) {
