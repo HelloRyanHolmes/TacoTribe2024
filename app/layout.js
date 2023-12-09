f8491ed40bba27a8fa5c8795936316aaeca58431
im-still-thinking <royaritra1990rocks@gmail.com>
2023-12-10 03:54:26 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -20,22 +20,25 @@ export const metadata = {
   title: 'Taco Tribe Shop',
   description: 'Taco taco moreee tacooo',
   openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
     images: [
       {
-        url: './opengraph-image.png',
-        width: 1920,
-        height: 1080
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
       },
     ],
-  },
-  twitter: {
-    images: [
-      {
-        url: './opengraph-image.png',
-        width: 1920,
-        height: 1080
-      },
-    ]
+    locale: 'en_US',
+    type: 'website',
   },
 }
 
