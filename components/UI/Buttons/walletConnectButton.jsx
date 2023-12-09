d89ba2869f0bb24f3c26c9e4fc99f3fe6a574ec3
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-12-09 13:18:18 +0530

    hover title added

--- components/UI/Buttons/walletConnectButton.jsx
+++ components/UI/Buttons/walletConnectButton.jsx
@@ -80,7 +80,7 @@ export const WalletConnectButton = () => {
                     )}
                     {/* {chain.name} */}
                   </button>
-                  <button onClick={openAccountModal} type="button" className='bg-yellow-400 border-2 border-black text-black rounded-full px-3 py-1 shadow-xl transform transition duration-300 ease-in-out shadow-black/20 hover:-translate-y-1 hover:bg-yellow-200'>
+                  <button title='Click to view address' onClick={openAccountModal} type="button" className='bg-yellow-400 border-2 border-black text-black rounded-full px-3 py-1 shadow-xl transform transition duration-300 ease-in-out shadow-black/20 hover:-translate-y-1 hover:bg-yellow-200'>
                     {/* {account.displayName} */}
                     {account.displayBalance
                       ? ` ${account.displayBalance}`
