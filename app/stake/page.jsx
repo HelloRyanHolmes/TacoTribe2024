4d86286b15733d665a8d959d116205e4bbca2e80
Risav03 <needleweed03@gmail.com>
2023-12-10 13:26:13 +0530

    done

--- app/stake/page.jsx
+++ app/stake/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import StakeTemplate from "../../components/UI/template/stakeTemplate"
 const banner = "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/banner.png";
 
 const taco1 = "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/StakingDappArt.png"
@@ -26,15 +27,8 @@ export default function Stake() {
           <Image width={1920} height={1080} src={banner} className="object-cover h-full" />
         </div>
       <main className=" w-screen h-screen flex flex-col items-center justify-center -mt-48">
-        <div className="w-[60%] bg-yellow-400 grid grid-cols-2 gap-5 p-5 py-10 rounded-[32px]">
-          <Image width={500} height={500} src={taco1} className=" w-full pl-10" />
-          <div className="flex flex-col gap-2 h-fit w-[80%] mx-auto my-auto">
-            <div className="bg-white rounded-full w-full px-4 py-2 shadow shadow-black/20 text-black text-xl"><h2 >Taco Tribe</h2></div>
-            <div className="bg-white rounded-full w-full px-4 py-2 shadow shadow-black/20 text-black text-xl"><h2 >Available Tacos: 0</h2></div>
-            <div className="w-fit py-1 text-[#73851C] text-3xl"><h2 >Stake your Tacos and earn $GUAC</h2></div>
-            <div className="bg-white rounded-full w-fit px-4 py-1 shadow shadow-black/20 text-black cursor-pointer hover:bg-white/80"><h2 >Learn More</h2></div>
-          </div>
-        </div>
+        
+        <StakeTemplate img={taco1} name="Taco Tribe" available="10"/>
 
         <div className="flex flex-row gap-6 mt-8">
           <button className='group cursor-pointer'>
