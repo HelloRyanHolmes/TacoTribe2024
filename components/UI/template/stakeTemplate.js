852f2ff7e54b6fde7c4ec5efc31b54cb0774b0e1
im-still-thinking <royaritra1990rocks@gmail.com>
2023-12-11 16:23:15 +0530

    some more cases added to staking

--- components/UI/template/stakeTemplate.js
+++ components/UI/template/stakeTemplate.js
@@ -21,10 +21,11 @@ export default function StakeTemplate({ name }) {
   const getContractDetails = async () => {
     switch (name) {
       case "Taco Tribe":
+        setImg("")
         return "Taco Tribe"
       case "Pixel Taco":
+        setImg("")
         return await pixelMintSetup()
-        // break
       case "Doodled Taco":
         setImg(doodle)
         return await doodledTacoMintSetup()
@@ -33,10 +34,14 @@ export default function StakeTemplate({ name }) {
         setImg(guacos)
         return "Guaco Tribe"
       case "Guac VS Sour Cream":
+        setImg("")
         return "Guac VS Sour Cream"
       case "Pixel Doodled Taco":
+        setImg("")
         return await doodledPixelTacoMintSetup()
-        break
+      case "":
+        setImg("")
+        return "No Taco Selected"
       default:
         return "Taco Tribe"
     }
@@ -47,7 +52,12 @@ export default function StakeTemplate({ name }) {
       const data = await getContractDetails()
       console.log(data)
       // setContract(data)
-      setBalance(await data?.balanceOf(address))
+      if(data === "No Taco Selected"){
+        setBalance("N/A")
+      }
+      else{
+        typeof(data) !== 'string' ? setBalance(await data?.balanceOf(address)) : setBalance(10)
+      }
     }
     fetchData()
   }, [name])
@@ -56,7 +66,7 @@ export default function StakeTemplate({ name }) {
     <div className="w-[60%] bg-yellow-400 grid grid-cols-2 gap-5 p-5 py-10 rounded-[32px]">
       <Image width={500} height={500} src={img} className=" w-full pl-10" />
       <div className="flex flex-col gap-2 h-fit w-[80%] mx-auto my-auto">
-        <div className="bg-white rounded-full w-full px-4 py-2 shadow shadow-black/20 text-black text-xl"><h2 >{name}</h2></div>
+        <div className="bg-white rounded-full w-full px-4 py-2 shadow shadow-black/20 text-black text-xl"><h2 >{name?.length === 0 ? 'Select A Taco' : name}</h2></div>
         <div className="bg-white rounded-full w-full px-4 py-2 shadow shadow-black/20 text-black text-xl"><h2 >Available Tacos: {Number(balance)}</h2></div>
         <div className="w-fit py-1 text-[#73851C] text-3xl"><h2 >Stake your Tacos and earn $GUAC</h2></div>
         <div className="bg-white rounded-full w-fit px-4 py-1 shadow shadow-black/20 text-black cursor-pointer hover:bg-white/80"><h2 >Learn More</h2></div>
