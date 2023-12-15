e3f8190623537bb160bba3fb7e9fc6f4113282aa
Risav03 <needleweed03@gmail.com>
2023-12-15 16:07:16 +0530

    fixed loader

--- components/UI/Loader/Loader.jsx
+++ components/UI/Loader/Loader.jsx
@@ -10,11 +10,11 @@ const Loader = () => {
 
   if(loader) return (
     <div className='absolute top-0 left-0 '>
-        <div className='absolute w-screen h-screen flex flex-col z-[9999] bg-yellow-500/80 items-center justify-center top-0 left-0'>
+        <div className='fixed w-screen h-screen flex flex-col z-[9999] bg-yellow-500/80 items-center justify-center top-0 left-0'>
             <Image src={loaderGif} width={800} height={800} alt="" className='w-80'/>
             <h3 className='text-5xl text-black'>Loading...</h3>
         </div>
-        <div className='absolute w-screen h-screen top-0 left-0 backdrop-blur-sm blur-sm z-[9998]'></div>
+        <div className='fixed w-screen h-screen top-0 left-0 backdrop-blur-sm blur-sm z-[9998]'></div>
     </div>
   )
 }
