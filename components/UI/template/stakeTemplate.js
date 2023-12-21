063cd848376444aee78b74badc3094a889b70a56
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-12-22 01:14:22 +0530

    minor ui change

--- components/UI/template/stakeTemplate.js
+++ components/UI/template/stakeTemplate.js
@@ -681,17 +681,20 @@ export default function StakeTemplate({ tacoType }) {
   return (
     <div>
       <div className="w-[95%] md:w-[700px] mx-auto bg-yellow-400 mb-10 overflow-hidden items-center justify-center grid grid-cols-2 max-md:grid-flow-row max-md:grid-cols-1 gap-x-5 p-5 rounded-[32px]">
-        <div className="h-80 my-auto flex items-center justify-center"><Image width={500} height={500} src={img} className=" object-cover object-center w-[90%] sm:pl-10" /></div>
+        <div className="h-80 my-auto flex flex-col items-center justify-center">
+          <Image width={500} height={500} src={img} className=" object-cover object-center w-[90%]" />
+          {balance > 0 && tacoType == 1 || tacoType == 0 && <button onClick={claimAll} className='group cursor-pointer mx-auto max-md:mt-5 md:col-span-2'>
+          <Image width={80} height={80} src={claimUp} alt="home" className={"w-40 group-hover:hidden"} />
+          <Image width={80} height={80} src={claimDown} alt="home" className={"w-40 hidden group-hover:block"} />
+        </button>}
+        </div>
         <div className="flex flex-col max-md:text-center max-md:items-center gap-2 h-fit w-[80%] mx-auto my-auto">
           <div className="bg-white rounded-full w-full px-4 py-2 shadow shadow-black/20 text-black text-xl"><h2 >{nameArr[tacoType]}</h2></div>
           <div className="bg-white rounded-full w-full px-4 py-2 shadow shadow-black/20 text-black text-xl"><h2 >Available Tacos: {Number(balance)}</h2></div>
           <div className="w-fit py-1 text-[#73851C] text-3xl"><h2 >Stake your Tacos and earn $GUAC</h2></div>
           <div className="bg-white rounded-full w-fit px-4 py-1 shadow shadow-black/20 text-black cursor-pointer hover:bg-white/80"><h2 >Learn More</h2></div>
         </div>
-        {balance > 0 && tacoType == 1 || tacoType == 0 && <button onClick={claimAll} className='group cursor-pointer mx-auto max-md:mt-5 md:col-span-2'>
-          <Image width={80} height={80} src={claimUp} alt="home" className={"w-40 group-hover:hidden"} />
-          <Image width={80} height={80} src={claimDown} alt="home" className={"w-40 hidden group-hover:block"} />
-        </button>}
+        
       </div>
 
       <div className="border-2 border-white bg-white mx-auto w-screen py-5 flex gap-5 px-5 items-center justify-center text-center">
