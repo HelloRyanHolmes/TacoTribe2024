af71c634fc32c65f84a0a3b466745d5bad09eabe
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-12-26 22:31:12 +0530

    day night tacos

--- app/page.js
+++ app/page.js
@@ -1,27 +1,39 @@
+"use client"
 import Image from "next/image";
 
 const bg = "https://d19rxn9gjbwl25.cloudfront.net/projectImages/taco_collection/taco-truck-night.png";
+const bgConnected = "https://tacotribe.s3.ap-south-1.amazonaws.com/assets/projectImages/taco_collection/tacoOpen.png";
 const bgMobile = "https://d19rxn9gjbwl25.cloudfront.net/projectImages/taco_collection/taco-truck-night-mobile.png";
+const bgMobileConnected = "https://tacotribe.s3.ap-south-1.amazonaws.com/assets/projectImages/taco_collection/tacoOpenMobile.png";
+
+import TacoMint from "../components/UI/Buttons/Minting/tacos"
+import { useEffect, useState } from "react";
+import { useAccount } from 'wagmi'
 
 export default function Home() {
+
+  const { isConnected, address } = useAccount()
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [isConnected])
+
   return (
     <>
       
       {/* Background */}
       <div className="absolute top-0 left-0 w-screen h-screen z-0 md:block max-h-screen overflow-hidden">
         <div className="relative h-full">
-          <Image width={1920} height={1080} src={bg} className="object-cover h-full" />
-        </div>
-      </div>
-
-      <div className="absolute top-0 left-0 w-screen h-screen z-0 sm:hidden max-h-screen overflow-hidden">
-        <div className="relative h-full">
-          <Image width={1920} height={1080} src={bgMobile} className="object-cover h-full" />
+          {!isConnected && isClient && <Image width={1920} height={1080} src={bg} className="object-cover max-sm:hidden h-full" />}
+          {isConnected && isClient && <Image width={1920} height={1080} src={bgConnected} className="object-cover max-sm:hidden h-full scale-110" />}
+          {!isConnected && isClient && <Image width={1920} height={1080} src={bgMobile} className="object-cover sm:hidden h-full" />}
+          {isConnected && isClient && <Image width={1920} height={1080} src={bgMobileConnected} className="object-cover sm:hidden h-full" />}
         </div>
       </div>
 
       <main className=" w-screen h-screen relative">
-        
+        <TacoMint/>
       </main>
 
     </>
--- components/UI/Buttons/Minting/tacos.jsx
+++ components/UI/Buttons/Minting/tacos.jsx
@@ -9,8 +9,9 @@ import abi from "../../../../utils/newAbis/tacotribeabi"
 import { useAccount } from 'wagmi'
 import Swal from 'sweetalert2'
 
-const claimUp = "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/Tan+Button+UP.png"
-const claimDown = "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/Tan+Button+DOWN.png"
+const claimUp = "https://tacotribe.s3.ap-south-1.amazonaws.com/assets/buttons/Mint_Button_UP.png"
+const claimDown = "https://tacotribe.s3.ap-south-1.amazonaws.com/assets/buttons/Mint_Button_DOWN.png"
+
 
 const error = "https://tacotribe.s3.ap-south-1.amazonaws.com/assets/ui/error.png"
 
@@ -53,14 +54,13 @@ export default function TacoMint() {
 
     const [amount, setAmount] = useState(0);
     const [amountBoxShow, setAmountBoxShow] = useState(false);
-    const { address } = useAccount()
+    const { address, isConnected } = useAccount()
 
 
     async function mint() {
         const contract = await tacoMintSetup(address);
         console.log("inside mint", contract);
-        t
-        await contract.mint(amount, {value: ethers.utils.parseEther(String(25 * amount)) }).then(
+        await contract.mint(amount, {value: ethers.utils.parseEther(String(15 * amount)) }).then(
             (res) => {
                 console.log(res);
                 Swal.fire({
@@ -89,6 +89,9 @@ export default function TacoMint() {
                         })
                     });
             }
+        ).catch((err)=>{
+            console.log("Error", err);
+        }
         )
 
     }
@@ -99,15 +102,16 @@ export default function TacoMint() {
 
     return (
         <>
-            <button onClick={() => { setAmountBoxShow(true) }} className=" max-[950px]:hidden block absolute top-1/2 xl:right-[10%] right-[5%] max-[1100px]:right-0 opacity-0 cursor-pointer hover:animate-pulse hover:bg-white/20 rounded-3xl w-[25%] h-[20%]"></button>
+            <button onClick={() => { isConnected && setAmountBoxShow(true) }} className=" absolute bg-transparent z-10 max-[950px]:hidden w-screen h-screen"></button>
 
-            <button onClick={() => { setAmountBoxShow(true) }} className=' hidden max-[950px]:block group cursor-pointer absolute z-10 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'>
+            <button onClick={() => { isConnected && setAmountBoxShow(true) }} className={` ${!isConnected && " max-md:hidden "} md:hidden max-[950px]:block group cursor-pointer absolute z-20 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2`}>
                 <Image width={80} height={80} src={claimUp} alt="home" className={"w-40 group-hover:hidden"} />
                 <Image width={80} height={80} src={claimDown} alt="home" className={"w-40 hidden group-hover:block"} />
             </button>
+            
 
             {amountBoxShow &&
-                <div className="bg-yellow-400 z-10 border-2 border-black rounded-2xl w-[300px] px-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-2xl shadow-black">
+                <div className="bg-yellow-400 z-20 border-2 border-black rounded-2xl w-[300px] px-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-2xl shadow-black">
                     <div className="relative flex flex-col items-center justify-center w-full h-full p-5 pt-10">
                         <h2 onClick={() => { setAmountBoxShow(false) }} className="absolute top-0 right-0 cursor-pointer m-2 mx-4 text-black hover:text-red-600 transform hover:scale-125 transition-all duration-200 ease-in-out">x</h2>
                         <input placeholder="0" type="number" onKeyDown={(e) => { e.preventDefault() }} step={1} min={0} onChange={handleamountChange} value={amount} className="text-black border-2 border-black p-5 py-4 text-center text-3xl block h-fit w-full rounded-xl">
