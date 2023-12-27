e475b5997db728aed2586c69db9b4c02240c6c56
im-still-thinking <royaritra1990rocks@gmail.com>
2023-12-27 07:55:59 +0530

    raffle and minimart update

--- EDITABLES/minimartData.js
+++ EDITABLES/minimartData.js
@@ -1,20 +1,20 @@
 export const minimartData = [
-    {
-      id: "#456",
-      name: "SQUIBLETS X FRENZ ",
-      cost: 2000,
-      img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png"
-    },
-    {
-      id: "#543",
-      name: "SQUIBLETS X FRENZ ",
-      cost: 2500,
-      img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png"
-    },
-    {
-      id: "#643",
-      name: "SQUIBLETS X FRENZ ",
-      cost: 3500,
-      img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png"
-    }
+    // {
+    //   id: "#456",
+    //   name: "SQUIBLETS X FRENZ ",
+    //   cost: 2000,
+    //   img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png"
+    // },
+    // {
+    //   id: "#543",
+    //   name: "SQUIBLETS X FRENZ ",
+    //   cost: 2500,
+    //   img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png"
+    // },
+    // {
+    //   id: "#643",
+    //   name: "SQUIBLETS X FRENZ ",
+    //   cost: 3500,
+    //   img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png"
+    // }
   ]
--- EDITABLES/raffleData.js
+++ EDITABLES/raffleData.js
@@ -1,23 +1,23 @@
 export const raffleData = [
-    {
-      id: "#456",
-      name: "SQUIBLETS X FRENZ ",
-      link: "",
-      img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png",
-      buttonText: "Hola!"
-    },
-    {
-      id: "#543",
-      name: "SQUIBLETS X FRENZ ",
-      link: "",
-      img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png",
-      buttonText: "Hehe!"
-    },
-    {
-      id: "#223",
-      name: "SQUIBLETS X FRENZ ",
-      link: "",
-      img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png",
-      buttonText: "Ohh!"
-    }
+    // {
+    //   id: "#456",
+    //   name: "SQUIBLETS X FRENZ ",
+    //   link: "",
+    //   img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png",
+    //   buttonText: "Hola!"
+    // },
+    // {
+    //   id: "#543",
+    //   name: "SQUIBLETS X FRENZ ",
+    //   link: "",
+    //   img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png",
+    //   buttonText: "Hehe!"
+    // },
+    // {
+    //   id: "#223",
+    //   name: "SQUIBLETS X FRENZ ",
+    //   link: "",
+    //   img: "https://d19rxn9gjbwl25.cloudfront.net/projectImages/staking/guacos.png",
+    //   buttonText: "Ohh!"
+    // }
   ]
